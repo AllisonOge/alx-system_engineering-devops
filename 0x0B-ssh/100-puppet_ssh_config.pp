6aa97de0192850f0056edc54b335eea904d500ff
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2023-11-27 00:49:24 +0000

    task#5 attempt#2

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,20 +1,20 @@
 # Manifest to custom configure SSH file
-  exec { 'Turn off passwd auth':
-    command     => '/usr/bin/sed -i "s/^#*PasswordAuthentication.*/PasswordAuthentication no/" /etc/ssh/sshd_config',
-    refreshonly => true,
-    subscribe   => File['/etc/ssh/sshd_config'],
-  }
+file { '/etc/ssh/sshd_config':
+  ensure => file,
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0644',
+}
 
-  exec { 'Declare identity file':
-    command     => '/usr/bin/echo "IdentityFile ~/.ssh/school" >> /etc/ssh/sshd_config',
-    unless      => '/usr/bin/grep -q "^#*IdentityFile ~/.ssh/school" /etc/ssh/sshd_config',
-    refreshonly => true,
-    subscribe   => File['/etc/ssh/sshd_config'],
-  }
+exec { 'Turn off passwd auth':
+  command     => '/usr/bin/sed -i "s/^#*PasswordAuthentication.*/PasswordAuthentication no/" /etc/ssh/sshd_config',
+  refreshonly => true,
+  subscribe   => File['/etc/ssh/sshd_config'],
+}
 
-  file { '/etc/ssh/sshd_config':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-  }
+exec { 'Declare identity file':
+  command     => '/usr/bin/echo "IdentityFile ~/.ssh/school" >> /etc/ssh/sshd_config',
+  unless      => '/usr/bin/grep -q "^#*IdentityFile ~/.ssh/school" /etc/ssh/sshd_config',
+  refreshonly => true,
+  subscribe   => File['/etc/ssh/sshd_config'],
+}
