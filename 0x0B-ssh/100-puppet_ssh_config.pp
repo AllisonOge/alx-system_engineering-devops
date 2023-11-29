6f6599c0592e4b67b8c4a1e7a8f10865ca013919
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-11-28 20:46:17 -0800

    replaced sshd_config with ssh_config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,14 +3,14 @@ include stdlib
 
   file_line { 'Turn off passwd auth':
     ensure   => present,
-    path     => '/etc/ssh/sshd_config',
+    path     => '/etc/ssh/ssh_config',
     line     => 'PasswordAuthentication no',
     match    => '^\s*#?PasswordAuthentication',
   }
 
   file_line { 'Declare identity file':
     ensure => present,
-    path   => '/etc/ssh/sshd_config',
+    path   => '/etc/ssh/ssh_config',
     line   => 'IdentityFile ~/.ssh/school',
     match  => '^\s*#?IdentityFile',
  }
