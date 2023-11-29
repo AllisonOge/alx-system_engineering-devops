d120c9ad58c1662b76ac9ab99afd7a505ec28a37
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-11-29 15:55:20 -0800

    task#1 fix code in manifest

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,12 @@
 # Manifest to install flask from pip3
-  package {'python3-pip':
-    ensure => latest,
+ package {'Werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3',
+    before  => Package['Flask'], # ensure Werkzeug is installed before Flask
   }
 
-  package {'Werkzueg':
-    ensure          => '2.1.1',
-    provider        => 'pip3',
-    install_options => ['--executable', '/usr/bin/python3 -m pip'],
-    require         => Package['python3-pip'],
-  }
-
-  package {'flask':
+  package {'Flask':
     ensure   => '2.1.0', # ensures the package v2.1.0 is installed
     provider => 'pip3', # sets the provider to pip3 (optional)
-    require  => Package['python3-pip'],
+    require  => Package['Werkzeug'],
   }
