84bd41d2d4fb6d338f5270d8a1888e16e852412d
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-11-28 20:33:15 -0800

    task#5 puppet manifest to configure ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,15 +2,15 @@
 include stdlib
 
   file_line { 'Turn off passwd auth':
-    ensure => present,
-    path   => '/etc/ssh/sshd_config',
-    line   => 'PasswordAuthentication no',
-    match  => '^#?PasswordAuthentication',
+    ensure   => present,
+    path     => '/etc/ssh/sshd_config',
+    line     => 'PasswordAuthentication no',
+    match    => '^\s*#?PasswordAuthentication',
   }
 
   file_line { 'Declare identity file':
     ensure => present,
     path   => '/etc/ssh/sshd_config',
     line   => 'IdentityFile ~/.ssh/school',
-    match  => '^#?IdentityFile',
+    match  => '^\s*#?IdentityFile',
   }
