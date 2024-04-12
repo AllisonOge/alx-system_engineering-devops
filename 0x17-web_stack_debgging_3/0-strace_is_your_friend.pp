52a2479755a8ff0fe1db82ade5369c878003a44c
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-04-12 23:14:54 +0000

    task#0 final code

--- 0x17-web_stack_debgging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debgging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
 # fix typo error in /var/www/html/wp-settings.php
 exec {'fix-typo':
-    command => '/usr/bin/sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path    => '/usr/local/bin/:/bin/',
 }
