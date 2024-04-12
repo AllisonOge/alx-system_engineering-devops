4a531ccdac9ccdf0c90943825e5ece52d09c2b12
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2024-04-12 18:47:13 +0000

    automate nginx install and config

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,34 @@
+/*
+Automate the task of creating a custom HTTP header response
+with Puppet.
+
+Requirements:
+- The name of the custom HTTP header must be X-Served-By
+- The value of the custom HTTP header must be the hostname
+of the server Nginx is running on
+- Write 2-puppet_custom_http_response_header.pp so that it
+configures a brand new Ubuntu machine to the requirements
+*/
+
+# install and configure Nginx
+exec {'apt-update':
+    command => 'apt-get update',
+    path    => '/usr/bin/',
+}
+
+package {'nginx':
+    ensure  => installed,
+    require => Exec['apt-update'],
+}
+
+file_line {'X-Served-By':
+    path     => '/etc/nginx/sites-available/default',
+    match    => '^server {',
+    line     => "server {\n\tadd_header X-Served-By \"${hostname}\";",
+    multiple => false,
+    notify   => Exec['restart_nginx'],
+}
+
+exec {'restart_nginx':
+    command => '/usr/bin/service nginx restart',
+}
